825cb781bf61e80a512c59a392518d16870c1a3b
NewPlace2Frown <newplace2frown@gmail.com>
2023-06-15 01:02:25 +0100

    Create Gallery.js

---
+++ Gallery.js
@@ -0,0 +1,48 @@
+const slideshow = document.getElementById("slideshow");
+const photo = document.getElementById("photo");
+
+const photos = [];
+let currentPageName = window.location.pathname.split("/").pop(); // Extract current page name from URL
+
+for (let i = 1; i <= 135; i++) {
+  let num = i.toString().padStart(3, '0');
+  let filePath = `media/${currentPageName}/Photo${num}.jpeg`;
+  photos.push(filePath);
+}
+
+
+let currentPhoto = 0;
+photo.src = photos[currentPhoto];
+
+function nextPhoto() {
+  currentPhoto = (currentPhoto + 1) % photos.length;
+  photo.src = photos[currentPhoto];
+}
+
+function prevPhoto() {
+  currentPhoto = (currentPhoto - 1 + photos.length) % photos.length;
+  photo.src = photos[currentPhoto];
+}
+
+slideshow.addEventListener("click", nextPhoto);
+
+window.addEventListener("keydown", function(event) {
+  switch(event.keyCode) {
+    case 37: // Left arrow
+      prevPhoto();
+      break;
+    case 39: // Right arrow
+      nextPhoto();
+      break;
+  }
+});
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
