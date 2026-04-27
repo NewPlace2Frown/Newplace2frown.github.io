f6f9566fa3d9dc33035b93c8036c7483db851955
sub-surface <leontakesphotos@gmail.com>
2026-04-27 19:58:43 +0100

    feat(data): add site.js with nav, socials, shop URL

---
+++ src/_data/site.js
@@ -0,0 +1,24 @@
+export default {
+  title: 'NewPlace2Frown',
+  tagline: 'Documentary photography by Leon Morgan',
+  url: 'https://newplace2frown.com',
+  shopUrl: 'https://shop.newplace2frown.com',
+  author: {
+    name: 'Leon Morgan',
+    email: 'newplace2frown@gmail.com'
+  },
+  nav: [
+    { label: 'Work',     href: '/work/' },
+    { label: 'Projects', href: '/projects/' },
+    { label: 'Journal',  href: '/journal/' },
+    { label: 'Prints',   href: '/prints/' },
+    { label: 'About',    href: '/about/' },
+    { label: 'Contact',  href: '/contact/' }
+  ],
+  socials: [
+    { label: 'Instagram', href: 'https://instagram.com/le.on_photos' },
+    { label: 'Twitter',   href: 'https://twitter.com/newplace2frown' },
+    { label: 'TikTok',    href: 'https://www.tiktok.com/@newplace2frown' },
+    { label: 'SoundCloud', href: 'https://soundcloud.com/m0rvidd' }
+  ]
+};
